88f5cd21298d755e970956cbbb7eecb5bf769f6e
GabrielSpaniolSU <paulafernandes4658@gmail.com>
2024-11-13 23:48:10 -0300

    update

--- src/main/java/com/example/sistemadeimobliaria/dto/ClienteDto.java
+++ src/main/java/com/example/sistemadeimobliaria/dto/ClienteDto.java
@@ -16,13 +16,18 @@ public class ClienteDto {
     @Schema(description = "Cpf do cliente", example = "123.545.643-51", title = "CPF" )
     private String cpf;
 
+    @Schema(description = "Email do Cliente", example = "joaopaulu@gmail.com", title = "Email" )
     private String email;
 
+    @Schema(description = "Telefone do cliente", example = "(46)99740829", title = "Telefone" )
     private String telefone;
 
+    @Schema(description = "Endereço do cliente", example = "229", title = "Endereço" )
     private String endereco;
 
+    @Schema(description = "Data de cadastro do cliente", example = "15/10/2007", title = "Data de Cadastro")
     private String dataCadastro;
 
+    @Schema(description = "Contrato do cliente", example = "56°", title = "Contrato" )
     private ContratoDto contrato;
 }
